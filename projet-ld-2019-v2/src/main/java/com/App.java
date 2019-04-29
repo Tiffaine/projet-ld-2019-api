d03500dcc5a3b5705b4b1f680ad52201bcfb0bea
@vicdauvi <victor.daudrevignier@reseau.eseo.fr>
2019-04-29 19:11:50 +0200

    test commit n°2

--- projet-ld-2019-v2/src/main/java/com/App.java
+++ projet-ld-2019-v2/src/main/java/com/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.controller.SujetController;
-//TestCommit
+//TestCommitv2
 @SpringBootApplication
 @ComponentScan(basePackageClasses=SujetController.class)
 public class App 
