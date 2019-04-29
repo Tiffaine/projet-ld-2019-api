64229be4833cb7ac6ef1cf6fe6011a250f18d9a9
alban-deb <alban.debarre@reseau.eseo.fr>
2019-04-29 19:43:42 +0200

    Changement dans SujetDao

--- projet-ld-2019-v2/src/main/java/com/dao/SujetDao.java
+++ projet-ld-2019-v2/src/main/java/com/dao/SujetDao.java
@@ -17,8 +17,7 @@ public class SujetDao {
 	private static String selectSubject = "SELECT titre, confidentialite from `sujet` where idSujet=?";
 	private static String selectSubjectDescription = "SELECT description from `sujet` where idSujet=?";
 	private static String selectPosterWhereSubject = "SELECT idPoster from `poster` where idSujet=?";
-	private static String selectProfesseurSujetWhereSubject = "SELECT idProfesseur from `professeursujet` where idSujet=?";
-	private static String selectEquipeWhereSubject = "SELECT idEquipe from `equipe` where idSujet=?";
+	private static String SQL_selectSujetsProffesseur_DEBUT = "SELECT s.* FROM (sujet s, professeur p, professeursujet ps) WHERE s.idSujet = ps.idSujet AND p.idProfesseur = ps.idProfesseur AND p.idProfesseur=";
 	private static String url = "jdbc:mysql://localhost/somanager?useLegacyDatetimeCode=false&serverTimezone=Europe/Paris&useSSL=false";
 	private static String user = "root";
 	private static String pwd = "";
@@ -46,10 +45,24 @@ public class SujetDao {
 		List<Sujet> listeSujets = new ArrayList<Sujet>();
 		Statement stmt = connect.createStatement();
 		ResultSet result = stmt.executeQuery(selectAll);
+		
+		
 		while (result.next()) {
-			listeSujets.add(new Sujet(result.getString("idSujet"), result.getString("titre"),
-					result.getString("description"), result.getString("nbrMinEleves"), result.getString("nbrMaxEleves"),
-					result.getString("contratPro"), result.getString("confidentialite"), result.getString("etat")));
+			Sujet sujet = new Sujet();
+			sujet.setConfidentialite(result.getBoolean("confidentialite"));
+			sujet.setContratPro(result.getBoolean("contratPro"));
+			sujet.setDescription(result.getString("description"));
+			sujet.setEtatString((result.getString("etat")));
+			sujet.setIdentifiant(result.getString("identifiant"));
+			sujet.setIdSujet(result.getLong("idSujet"));
+			sujet.setInterets(result.getString("interets"));
+			sujet.setLiens(result.getString("liens"));
+			sujet.setNbrMaxEleves(result.getInt("nbrMaxEleves"));
+			sujet.setNbrMinEleves(result.getInt("nbrMinEleves"));
+			sujet.setNoteInteretSujet(result.getFloat("noteInteretSujet"));
+			sujet.setTitre("titre");
+			
+			listeSujets.add(sujet);
 		}
 		return listeSujets;
 	}
@@ -73,115 +86,69 @@ public class SujetDao {
 		return newSQL.toString().replaceAll("\"null\"", "null");
 	}
 
-	
-	public List<String> selectASubject(String idSujet, String identifiantUser) throws SQLException {
+	public List<String> selectASubject(String idSujet) throws SQLException {
 		List<String> listeInformations = new ArrayList<String>();
 		PreparedStatement preparedStatement = null;
 		PreparedStatement preparedStatement2 = null;
 		PreparedStatement preparedStatement3 = null;
-		PreparedStatement preparedStatement4 = null;
-		PreparedStatement preparedStatement6 = null;
-		boolean lieAuProjet = false; 
-			
 		preparedStatement = (PreparedStatement) connect
 				.prepareStatement(initialisationRequetePreparee(selectSubject, idSujet));
 		ResultSet result = preparedStatement.executeQuery();
-		listeInformations.add(" idSujet ");
 		listeInformations.add(idSujet);
 		result.next();
-		listeInformations.add("titre");
 		listeInformations.add(result.getString("titre"));
-		listeInformations.add(" niveau confidentialite ");
 		listeInformations.add(result.getString("confidentialite"));
+		
+		//Le descriptif seulement si confidentialite = 1
+		if (result.getString("confidentialite").contentEquals("0")) {
+			preparedStatement2 = (PreparedStatement) connect
+					.prepareStatement(initialisationRequetePreparee(selectSubjectDescription, idSujet));
+			ResultSet result2 = preparedStatement2.executeQuery();
+			result2.next();
+			listeInformations.add(result2.getString("description"));
+		}
+		//Le descriptif si l'utilisateur est lié au sujet 
+		System.out.println("Milieu"+listeInformations);
 
-		// Si oui ou non un poster a ete depose
+		//Si oui ou non un poster a ete depose 
 		preparedStatement3 = (PreparedStatement) connect
 				.prepareStatement(initialisationRequetePreparee(selectPosterWhereSubject, idSujet));
 		ResultSet result3 = preparedStatement3.executeQuery();
 		if (result3 == null) {
 			listeInformations.add("NoPoster");
-		} else {
+		}
+		else {
 			listeInformations.add("PosterDepose");
 		}
+		System.out.println("Fin"+listeInformations);
 
-		// Nom du prof qui suit le projet
-		preparedStatement4 = (PreparedStatement) connect
-				.prepareStatement(initialisationRequetePreparee(selectProfesseurSujetWhereSubject, idSujet));
-		ResultSet result4 = preparedStatement4.executeQuery();
-		result4.next();
-		String idProfesseur = result4.getString("idProfesseur");
-		if(idProfesseur==identifiantUser) {
-			lieAuProjet=true; 
-		}
-		String requeteProf = "SELECT nom,prenom from `utilisateur` where idUtilisateur=" + idProfesseur;
-		Statement stmt = connect.createStatement();
-		ResultSet result5 = stmt.executeQuery(requeteProf);
-		result5.next();
-		listeInformations.add(" Professeur qui suit le projet : ");
-		listeInformations.add(result5.getString("nom"));
-		listeInformations.add(result5.getString("prenom"));
-
-		// Nom des etudiants sur le projet
-		preparedStatement6 = (PreparedStatement) connect
-				.prepareStatement(initialisationRequetePreparee(selectEquipeWhereSubject, idSujet));
-		ResultSet result6 = preparedStatement6.executeQuery();
-		result6.next();
-		String idEquipe = result6.getString("idEquipe");
-
-		String requeteEquipe = "SELECT idEtudiant from `EtudiantEquipe` where idEquipe=\"" + idEquipe + "\"";
-		ResultSet result7 = stmt.executeQuery(requeteEquipe);
-		String idEtudiant=null;
-		List<String> listeidEtudiant = new ArrayList<String>();
-		
-		while (result7.next()) {
-			listeidEtudiant.add(result7.getString("idEtudiant"));
-		}
-		for (int j=0;j<listeidEtudiant.size();j++){
-			if(listeidEtudiant.get(j)==identifiantUser) {
-				lieAuProjet=true; 
-			}
-		}
-				
-		String requeteEtudiant = "SELECT nom,prenom from `utilisateur` where idUtilisateur=";
-		for (int i=0;i<listeidEtudiant.size();i++ ) {
-			if(i==0) {
-				requeteEtudiant= requeteEtudiant + listeidEtudiant.get(i);
-			}
-			else if(i>0 && i<listeidEtudiant.size()) {
-				requeteEtudiant= requeteEtudiant + " or idUtilisateur="+ listeidEtudiant.get(i);
-			}
-		}
-		ResultSet result8 = stmt.executeQuery(requeteEtudiant);
-		while(result8.next()) {
-			listeInformations.add(" Etudiant membre du projet : ");
-
-			listeInformations.add(result8.getString("nom"));
-			listeInformations.add(result8.getString("prenom"));
-		}
-		// Le descriptif seulement si confidentialite = 1
-				if (result.getString("confidentialite").contentEquals("0")||lieAuProjet==true) {
-					preparedStatement2 = (PreparedStatement) connect
-							.prepareStatement(initialisationRequetePreparee(selectSubjectDescription, idSujet));
-					ResultSet result2 = preparedStatement2.executeQuery();
-					result2.next();
-					listeInformations.add(" Description sujet : ");
-
-					listeInformations.add(result2.getString("description"));
-				}
-		
 		return listeInformations;
 	}
-	
-	public List<String> selectSujetFromJury(String idProf){
-		List<String> listeInformations = new ArrayList<String>();
-		
-		//Check que l'utilisateur est un prof
-		
-		//Récupération des infos
-		
-		//return
-		return null;
-		
-	}
 
+	public List<Sujet> selectSujetsProffesseur(String idProfesseur) throws SQLException {
+		List<Sujet> listeSujets = new ArrayList<Sujet>();
+		Statement stmt = connect.createStatement();
+		ResultSet result = stmt.executeQuery(SQL_selectSujetsProffesseur_DEBUT + idProfesseur);
+		while (result.next()) {
+			Sujet sujet = new Sujet();
+			
+			
+			sujet.setConfidentialite(result.getBoolean("confidentialite"));
+			sujet.setContratPro(result.getBoolean("contratPro"));
+			sujet.setDescription(result.getString("description"));
+			sujet.setEtatString((result.getString("etat")));
+			sujet.setIdentifiant(result.getString("identifiant"));
+			sujet.setIdSujet(result.getLong("idSujet"));
+			sujet.setInterets(result.getString("interets"));
+			sujet.setLiens(result.getString("liens"));
+			sujet.setNbrMaxEleves(result.getInt("nbrMaxEleves"));
+			sujet.setNbrMinEleves(result.getInt("nbrMinEleves"));
+			sujet.setNoteInteretSujet(result.getFloat("noteInteretSujet"));
+			sujet.setTitre("titre");
+			
+			listeSujets.add(sujet);
+		}
+		return listeSujets;
+	}
+	
 }
--- projet-ld-2019-v2/src/main/java/com/model/Sujet.java
+++ projet-ld-2019-v2/src/main/java/com/model/Sujet.java
@@ -1,91 +1,300 @@
 package com.model;
 
+/**
+ * Classe du bean Sujet.
+ * 
+ * <p>D�finition des attributs de l'objet Sujet ainsi que des accesseurs et des mutateurs.</p>
+ */
 public class Sujet {
-	String idSujet; 
-	String titre; 
-	String description; 
-	String nbrMinEleves; 
-	String nbrMaxEleves; 
-	String contratPro; 
-	String confidentialite; 
-	String etat; 
+
+	/* Attributs */
+	private Long idSujet;
+	private String identifiant;
+	private String titre;
+	private String description;
+	private Integer nbrMinEleves;
+	private Integer nbrMaxEleves;
+	private Boolean contratPro;
+	private Boolean confidentialite;
+	private String optionsConcernees;
+	private EtatSujet etat;
+	private String liens;
+	private String interets;
+	private Float noteInteretTechno;
+	private Float noteInteretSujet;
 	
-	public Sujet(String idSujet,String titre, String description, String nbrMinEleves, String nbrMaxEleves, String contratPro, String confidentialite, String etat ) {
-		super(); 
-		this.idSujet=idSujet; 
-		this.titre=titre;
-		this.description=description; 
-		this.nbrMinEleves=nbrMinEleves; 
-		this.nbrMaxEleves=nbrMaxEleves; 
-		this.contratPro=contratPro; 
-		this.confidentialite=confidentialite; 
-		this.etat=etat; 
+	/**
+	 * Constructeur par d�faut.
+	 */
+	public Sujet() {
+		super();
 	}
 
-	public String getIdSujet() {
-		return idSujet;
+	public String getIdentifiant() {
+		return identifiant;
 	}
 
-	public void setIdSujet(String idSujet) {
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+
+	/* Accesseurs et Mutateurs */
+	/**
+	 * Accessseur pour r�cup�rer l'ID du sujet
+	 * @return idSujet
+	 */
+	public Long getIdSujet() {
+		return this.idSujet;
+	}
+
+	/**
+	 * Mutateur pour modifier l'ID du sujet
+	 * @param idSujet
+	 */
+	public void setIdSujet(Long idSujet) {
 		this.idSujet = idSujet;
 	}
 
+	/**
+	 * Accessseur pour r�cup�rer le titre du sujet
+	 * @return titre
+	 */
 	public String getTitre() {
-		return titre;
+		return this.titre;
 	}
 
+	/**
+	 * Mutateur pour modifier le titre du sujet 
+	 * @param titre
+	 */
 	public void setTitre(String titre) {
 		this.titre = titre;
 	}
 
+	/**
+	 * Accessseur pour r�cup�rer la description du sujet
+	 * @return description
+	 */
 	public String getDescription() {
-		return description;
+		return this.description;
 	}
 
+	/**
+	 * Mutateur pour modifier la description du sujet
+	 * @param description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	public String getNbrMinEleves() {
-		return nbrMinEleves;
+	/**
+	 * Accessseur pour r�cup�rer le nombre minimum d'�l�ves dans une �quipe sur ce sujet
+	 * @return nbrMinEleves
+	 */
+	public Integer getNbrMinEleves() {
+		return this.nbrMinEleves;
 	}
 
-	public void setNbrMinEleves(String nbrMinEleves) {
+	/**
+	 * Mutateur pour modifier le nombre minimum d'�l�ves dans une �quipe sur ce sujet
+	 * @param nbrMinEleves
+	 */
+	public void setNbrMinEleves(Integer nbrMinEleves) {
 		this.nbrMinEleves = nbrMinEleves;
 	}
 
-	public String getNbrMaxEleves() {
-		return nbrMaxEleves;
+	/**
+	 * Accessseur pour r�cup�rer le nombre maximum d'�l�ves dans une �quipe sur ce sujet
+	 * @return nbrMaxEleves
+	 */
+	public Integer getNbrMaxEleves() {
+		return this.nbrMaxEleves;
 	}
 
-	public void setNbrMaxEleves(String nbrMaxEleves) {
+	/**
+	 * Mutateur pour modifier le nombre maximum d'�l�ves dans une �quipe sur ce sujet
+	 * @param nbrMaxEleves
+	 */
+	public void setNbrMaxEleves(Integer nbrMaxEleves) {
 		this.nbrMaxEleves = nbrMaxEleves;
 	}
 
-	public String getContratPro() {
-		return contratPro;
+	/**
+	 * Accessseur pour r�cup�rer si le sujet est pour des �l�ves en contrat pro ou non
+	 * @return contratPro
+	 */
+	public Boolean getContratPro() {
+		return this.contratPro;
 	}
 
-	public void setContratPro(String contratPro) {
+	/**
+	 * Accessseur pour r�cup�rer si le sujet est pour des �l�ves en contrat pro ou non
+	 * sous forme d'un entier
+	 * @return contratPro avec une valeur de 0 ou 1 ou bien NULL
+	 */
+	public Integer getContratProInt() {
+		if (this.contratPro != null) {
+			return this.contratPro ? 1 : 0;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Mutateur pour modifier si le sujet est pour des �l�ves en contrat pro ou non
+	 * @param contratPro
+	 */
+	public void setContratPro(Boolean contratPro) {
 		this.contratPro = contratPro;
 	}
 
-	public String getConfidentialite() {
-		return confidentialite;
+	/**
+	 * Accessseur pour r�cup�rer si le sujet est confidentiel ou non
+	 * @return confidentialite
+	 */
+	public Boolean getConfidentialite() {
+		return this.confidentialite;
 	}
 
-	public void setConfidentialite(String confidentialite) {
+	/**
+	 * Accessseur pour r�cup�rer si le sujet est confidentiel ou non sous forme d'un entier
+	 * @return confidentialite avec une valeur de 0 ou 1 ou bien NULL
+	 */
+	public Integer getConfidentialiteInt() {
+		if (this.confidentialite != null) {
+			return this.confidentialite ? 1 : 0;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Mutateur pour modifier si le sujet est confidentiel ou non
+	 * @param confidentialite
+	 */
+	public void setConfidentialite(Boolean confidentialite) {
 		this.confidentialite = confidentialite;
 	}
 
-	public String getEtat() {
-		return etat;
+	/**
+	 * Accessseur pour r�cup�rer l'�tat du sujet
+	 * @return etat
+	 */
+	public EtatSujet getEtat() {
+		return this.etat;
 	}
 
-	public void setEtat(String etat) {
+	/**
+	 * Mutateur pour modifier l'�tat du sujet
+	 * @param etat
+	 */
+	public void setEtat(EtatSujet etat) {
 		this.etat = etat;
 	}
-	
-	
 
+	/**
+	 * Donne un �tat au Sujet.
+	 * 
+	 * @param etat l'�tat donn� au Sujet.
+	 */
+	public void setEtatString(String etat) {
+		if ("depose".equals(etat)) {
+			this.etat = EtatSujet.DEPOSE;
+		} else if ("valide".equals(etat)) {
+			this.etat = EtatSujet.VALIDE;
+		} else if ("attribue".equals(etat)) {
+			this.etat = EtatSujet.ATTRIBUE;
+		} else if ("refuse".equals(etat)) {
+			this.etat = EtatSujet.REFUSE;
+		} else if ("publie".equals(etat)) {
+			this.etat = EtatSujet.PUBLIE;
+		} else {
+			throw new BeanException("L'�tat entr� en param�tre n'existe pas !");
+		}
+	}
+
+	/**
+	 * Accessseur pour r�cup�rer les liens vers lesquels renvoie le sujet
+	 * @return liens
+	 */
+	public String getLiens() {
+		return this.liens;
+	}
+
+	/**
+	 * Mutateur pour modifier les liens vers lesquels renvoie le sujet
+	 * @param liens
+	 */
+	public void setLiens(String liens) {
+		this.liens = liens;
+	}
+
+	/**
+	 * Accessseur pour r�cup�rer les int�r�ts du sujet
+	 * @return interets
+	 */
+	public String getInterets() {
+		return this.interets;
+	}
+
+	/**
+	 * Mutateur pour modifier les int�r�ts du sujet
+	 * @param interets
+	 */
+	public void setInterets(String interets) {
+		this.interets = interets;
+	}
+
+	/**
+	 * Accessseur pour r�cup�rer la note d'int�r�t technologique du sujet
+	 * @return noteInteretTechno
+	 */
+	public Float getNoteInteretTechno() {
+		return this.noteInteretTechno;
+	}
+
+	/**
+	 * Mutateur pour modifier la note d'int�r�t technologique du sujet
+	 * @param noteInteretTechno
+	 */
+	public void setNoteInteretTechno(Float noteInteretTechno) {
+		this.noteInteretTechno = noteInteretTechno;
+	}
+
+	/**
+	 * Accessseur pour r�cup�rer la note d'int�r�t du sujet
+	 * @return noteInteretSujet
+	 */
+	public Float getNoteInteretSujet() {
+		return this.noteInteretSujet;
+	}
+
+	/**
+	 * Mutateur pour modifier la note d'int�r�t du sujet
+	 * @param noteInteretSujet
+	 */
+	public void setNoteInteretSujet(Float noteInteretSujet) {
+		this.noteInteretSujet = noteInteretSujet;
+	}
+	
+	/**
+	 * Red�finition de la m�thode toString().
+	 */
+	public String toString() {
+		return "Sujet [idSujet=" + this.idSujet + ", identifiant =" + this.identifiant +", titre=" + this.titre + ", description=" + this.description
+				+ ", nbrMinEleves=" + this.nbrMinEleves + ", nbrMaxEleves=" + this.nbrMaxEleves + ", contratPro="
+				+ this.contratPro + ", confidentialite=" + this.confidentialite + ", etat=" + this.etat + ", liens="
+				+ this.liens + ", interets=" + this.interets + ", noteInteretTechno=" + this.noteInteretTechno
+				+ ", noteInteretSujet=" + this.noteInteretSujet + "]";
+	}
+
+
+	public String getOptionsConcernees() {
+		return optionsConcernees;
+	}
+
+	public void setOptionsConcernees(String optionsConcernees) {
+		this.optionsConcernees = optionsConcernees;
+	}
+	
 }
